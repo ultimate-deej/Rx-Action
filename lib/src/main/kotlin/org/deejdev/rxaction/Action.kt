0888d941b0a85ef45ebd3a850186437cad2162c2
Maxim Naumov <ultimate.deej@gmail.com>
2018-12-20 22:58:48 +0300

    Minimal working `Action` implementation

--- lib/src/main/kotlin/org/deejdev/rxaction/Action.kt
+++ lib/src/main/kotlin/org/deejdev/rxaction/Action.kt
@@ -2,20 +2,41 @@ package org.deejdev.rxaction
 
 import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.functions.BiFunction
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
 
 class Action<Input, Output>(
     isUserEnabled: Observable<Boolean>,
-    execute: (Input) -> Single<Output>
+    private val execute: (Input) -> Single<Output>
 ) {
     constructor(execute: (Input) -> Single<Output>) : this(Observable.just(true), execute)
 
-    val values: Observable<Output> = TODO()
-    val errors: Observable<Throwable> = TODO()
-    val disabledErrors: Observable<Any> = TODO()
-    val isExecuting: Observable<Boolean> = TODO()
-    val isEnabled: Observable<Boolean> = TODO()
+    private val _values = PublishSubject.create<Output>()
+    private val _errors = PublishSubject.create<Throwable>()
+    private val _disabledErrors = PublishSubject.create<Any>()
+    private val _isExecuting = BehaviorSubject.createDefault(false)
+    private val _isEnabled = BehaviorSubject.create<Boolean>().also {
+        val combiner = BiFunction<Boolean, Boolean, Boolean> { userEnabled, executing -> userEnabled && !executing }
+        Observable.combineLatest(isUserEnabled, _isExecuting, combiner)
+            .subscribe(it)
+    }
+
+    val values: Observable<Output> = _values
+    val errors: Observable<Throwable> = _errors
+    val disabledErrors: Observable<Any> = _disabledErrors
+    val isExecuting: Observable<Boolean> = _isExecuting
+    val isEnabled: Observable<Boolean> = _isEnabled
 
     operator fun invoke(input: Input) {
-        TODO()
+        if (_isEnabled.value == false) {
+            _disabledErrors.onNext(Unit)
+            return
+        }
+
+        execute(input)
+            .doOnSubscribe { _isExecuting.onNext(true) }
+            .doFinally { _isExecuting.onNext(false) }
+            .subscribe(_values::onNext, _errors::onNext)
     }
 }
