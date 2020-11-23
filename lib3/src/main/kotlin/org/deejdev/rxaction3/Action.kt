59d2a71dbc8c9fc36dab79080dd1fb4e1018745b
Maxim Naumov <ultimate.deej@gmail.com>
2020-11-23 04:26:00 +0300

    RxJava 3 version

---
+++ lib3/src/main/kotlin/org/deejdev/rxaction3/Action.kt
@@ -0,0 +1,90 @@
+package org.deejdev.rxaction3
+
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.functions.BiFunction
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.PublishSubject
+
+class Action<Input, Output> private constructor(
+    isUserEnabled: Observable<Boolean>?,
+    private val execute: (Input) -> Observable<Output>
+) {
+    companion object {
+        @JvmStatic
+        fun <Input, Output> fromObservable(isUserEnabled: Observable<Boolean>, execute: (Input) -> Observable<Output>): Action<Input, Output> =
+            Action(isUserEnabled, execute)
+
+        @JvmStatic
+        fun <Input, Output> fromObservable(execute: (Input) -> Observable<Output>): Action<Input, Output> =
+            Action(null, execute)
+
+        @JvmStatic
+        fun <Input, Output> fromFlowable(isUserEnabled: Observable<Boolean>, execute: (Input) -> Flowable<Output>): Action<Input, Output> =
+            Action(isUserEnabled) { execute(it).toObservable() }
+
+        @JvmStatic
+        fun <Input, Output> fromFlowable(execute: (Input) -> Flowable<Output>): Action<Input, Output> =
+            Action(null) { execute(it).toObservable() }
+
+        @JvmStatic
+        fun <Input, Output> fromSingle(isUserEnabled: Observable<Boolean>, execute: (Input) -> Single<Output>): Action<Input, Output> =
+            Action(isUserEnabled) { execute(it).toObservable() }
+
+        @JvmStatic
+        fun <Input, Output> fromSingle(execute: (Input) -> Single<Output>): Action<Input, Output> =
+            Action(null) { execute(it).toObservable() }
+
+        @JvmStatic
+        fun <Input> fromCompletable(isUserEnabled: Observable<Boolean>, execute: (Input) -> Completable): Action<Input, Nothing> =
+            Action(isUserEnabled) { execute(it).toObservable() }
+
+        @JvmStatic
+        fun <Input> fromCompletable(execute: (Input) -> Completable): Action<Input, Nothing> =
+            Action(null) { execute(it).toObservable() }
+    }
+
+    private val _values = PublishSubject.create<Output>()
+    private val _errors = PublishSubject.create<Throwable>()
+    private val _completions = PublishSubject.create<Any>()
+    private val _disabledErrors = PublishSubject.create<Any>()
+    private val _isExecuting = BehaviorSubject.createDefault(false)
+    private val _isEnabled = BehaviorSubject.create<Boolean>().also {
+        if (isUserEnabled == null) {
+            _isExecuting
+                .map(Boolean::not)
+                .subscribe(it)
+        } else {
+            val combiner = BiFunction<Boolean, Boolean, Boolean> { userEnabled, executing -> userEnabled && !executing }
+            Observable.combineLatest(isUserEnabled.startWithItem(false), _isExecuting, combiner)
+                .subscribe(it)
+        }
+    }
+
+    val values: Observable<Output> = _values
+    val errors: Observable<Throwable> = _errors
+    val completions: Observable<Any> = _completions
+    val disabledErrors: Observable<Any> = _disabledErrors
+    val isExecuting: Observable<Boolean> = _isExecuting
+    val isEnabled: Observable<Boolean> = _isEnabled
+
+    val isExecutingValue: Boolean get() = _isExecuting.value!!
+    val isEnabledValue: Boolean get() = _isEnabled.value!!
+
+    operator fun invoke(input: Input) {
+        if (_isEnabled.value == false) {
+            _disabledErrors.onNext(Unit)
+            return
+        }
+
+        @Suppress("MoveLambdaOutsideParentheses")
+        execute(input)
+            .doOnSubscribe { _isExecuting.onNext(true) }
+            .doFinally { _isExecuting.onNext(false) }
+            .subscribe(_values::onNext, _errors::onNext, { _completions.onNext(Unit) })
+    }
+}
+
+operator fun <Output> Action<Unit, Output>.invoke() = invoke(Unit)
---
+++ lib3/src/test/kotlin/org/deejdev/rxaction3/ActionTest.kt
@@ -0,0 +1,162 @@
+package org.deejdev.rxaction3
+
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.TestScheduler
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+class ActionTest {
+    private lateinit var testScheduler: TestScheduler
+
+    @Before
+    fun initialize() {
+        testScheduler = TestScheduler()
+    }
+
+    @Test
+    fun `executes exactly once per invocation`() {
+        var numberOfExecutions = 0
+        val completable = Completable.create {
+            numberOfExecutions++
+            it.onComplete()
+        }
+        val action = Action.fromCompletable<Unit>(execute = { completable })
+
+        assertEquals("numberOfExecutions != 0", 0, numberOfExecutions)
+        action()
+        assertEquals("numberOfExecutions != 1", 1, numberOfExecutions)
+        action()
+        assertEquals("numberOfExecutions != 2", 2, numberOfExecutions)
+    }
+
+    @Test
+    fun `only executes when enabled`() {
+        val enabled = BehaviorSubject.create<Boolean>()
+        val single = Single.create<Unit> {
+            assertTrue(enabled.value!!)
+            it.onSuccess(Unit)
+        }
+        val action = Action.fromSingle<Unit, Unit>(enabled) { single }
+        val valuesObserver = action.values.test()
+
+        enabled.onNext(false)
+        action()
+        enabled.onNext(true)
+        action()
+        enabled.onNext(false)
+        action()
+
+        valuesObserver.assertValueCount(1)
+    }
+
+    @Test
+    fun `execution results forwarded to values`() {
+        val result = Any()
+        val action = Action.fromSingle<Unit, Any> { Single.just(result) }
+        val valuesObserver = action.values.test()
+
+        action()
+        valuesObserver.assertValuesOnly(result)
+    }
+
+    @Test
+    fun `execution errors forwarded to errors`() {
+        val error = RuntimeException()
+        val action = Action.fromSingle<Unit, Any> { Single.error(error) }
+        val errorsObserver = action.errors.test()
+
+        action()
+        errorsObserver.assertValuesOnly(error)
+    }
+
+    @Test
+    fun `execution errors do not terminate values`() {
+        val error = RuntimeException()
+        val action = Action.fromSingle<Unit, Any> { Single.error(error) }
+        val valuesObserver = action.values.test()
+
+        action()
+        valuesObserver.assertNotComplete().assertNoErrors()
+    }
+
+    @Test
+    fun `disabled action only emits disabled errors`() {
+        val enabled = Observable.just(false)
+        val action = Action.fromSingle<Unit, Any>(enabled) { Single.just(Unit) }
+        val valuesObserver = action.values.test()
+        val errorsObserver = action.errors.test()
+        val disabledErrorsObserver = action.disabledErrors.test()
+
+        action()
+        disabledErrorsObserver.assertValueCount(1)
+        action()
+        disabledErrorsObserver.assertValueCount(2)
+
+        valuesObserver.assertNoValues()
+        errorsObserver.assertNoValues()
+    }
+
+    @Test
+    fun isExecuting() {
+        val completable = Completable.complete()
+            .delay(100, TimeUnit.MILLISECONDS, testScheduler)
+        val action = Action.fromCompletable<Unit> { completable }
+
+        assertEquals(false, action.isExecutingValue)
+
+        action()
+        assertEquals(true, action.isExecutingValue)
+
+        testScheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS)
+        assertEquals(false, action.isExecutingValue)
+    }
+
+    @Test
+    fun `disabled while executing`() {
+        val completable = Completable.complete()
+            .delay(100, TimeUnit.MILLISECONDS, testScheduler)
+        val action = Action.fromCompletable<Unit> { completable }
+
+        assertEquals(true, action.isEnabledValue)
+
+        action()
+        assertEquals(false, action.isEnabledValue)
+
+        testScheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS)
+        assertEquals(true, action.isEnabledValue)
+    }
+
+    @Test
+    fun `won't execute in parallel`() {
+        val completable = Completable.complete()
+            .delay(100, TimeUnit.MILLISECONDS, testScheduler)
+        val action = Action.fromCompletable<Unit> { completable }
+        val completionsObserver = action.completions.test()
+        val disabledErrorsObserver = action.disabledErrors.test()
+
+        action()
+        action()
+        disabledErrorsObserver.assertValueCount(1)
+        completionsObserver.assertValueCount(0)
+
+        testScheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS)
+
+        completionsObserver.assertValueCount(1)
+    }
+
+    @Test
+    fun `multiple values per execution`() {
+        val observable = Observable.just(3, 2, 1)
+        val action = Action.fromObservable<Unit, Int> { observable }
+        val valuesObserver = action.values.test()
+
+        action()
+        valuesObserver.assertValuesOnly(3, 2, 1)
+    }
+}
